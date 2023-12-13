94149b3800a34b035b9a0403588fcfbb24ab579e
Mehrshad <code.rezaei@gmail.com>
2023-12-13 12:35:34 +0330

    build: run FSharpLint with 1 extra rule

--- build.fsx
+++ build.fsx
@@ -241,6 +241,7 @@ Target.create "SelfCheck" (fun _ ->
             "maxLinesInFunction"
             "maxLinesInMember"
             "maxLinesInConstructor"
+            "maxLinesInProperty"
         ]
 
     let jsonObj = JObject.Parse fsharplintJsonText
