67e9ddbbb01833ef90a02ff9944a1fcc9a5a4bf1
Andres G. Aragoneses <knocte@gmail.com>
2024-01-06 11:22:32 +0800

    build: run FSharpLint with 1 extra rule

--- build.fsx
+++ build.fsx
@@ -272,8 +272,8 @@ Target.create "SelfCheck" (fun _ ->
             "nestedStatements"
             (*
             "cyclomaticComplexity" rule is too complex and we can enable it later
-            "avoidSinglePipeOperator" rule must be improved and we can enable it later
             *)
+            "avoidSinglePipeOperator"
             "maxLinesInLambdaFunction"
             "maxLinesInMatchLambdaFunction"
             "maxLinesInValue"
