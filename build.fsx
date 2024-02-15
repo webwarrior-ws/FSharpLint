4aef3b38a38472cf1f6c1ea26499ff211775b21d
Mehrshad <code.rezaei@gmail.com>
2024-02-15 14:39:58 +0330

    scripts: convert `SelfCheck` FAKE target to fsx
    Co-authored-by: webwarrior <reg@webwarrior.ws>

--- build.fsx
+++ build.fsx
@@ -207,15 +207,6 @@ Target.create "Push" (fun _ ->
 
 )
 
-
-Target.create "SelfCheck" (fun _ ->
-    let srcDir = Path.Combine(rootDir.FullName, "src") |> DirectoryInfo
-
-    let consoleProj = Path.Combine(srcDir.FullName, "FSharpLint.Console", "FSharpLint.Console.fsproj") |> FileInfo
-    let sol = Path.Combine(rootDir.FullName, "FSharpLint.sln") |> FileInfo
-    exec "dotnet" (sprintf "run lint %s" sol.FullName) consoleProj.Directory.FullName
-)
-
 // --------------------------------------------------------------------------------------
 // Build order
 // --------------------------------------------------------------------------------------
