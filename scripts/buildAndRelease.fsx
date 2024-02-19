6f7d90e205840c695dbf2e983eb5d381c2d11ee3
Mehrshad <code.rezaei@gmail.com>
2024-02-19 12:54:24 +0330

    scripts/buildAndRelease: fix .targets not found
    To fix the error, we need to restore all paket packages:
    
    ```
    /home/runner/work/FSharpLint/FSharpLint/tests/FSharpLint.Core.Tests/FSharpLint.Core.Tests.fsproj(99,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
    /home/runner/work/FSharpLint/FSharpLint/tests/FSharpLint.FunctionalTest/FSharpLint.FunctionalTest.fsproj(17,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
    /home/runner/work/FSharpLint/FSharpLint/src/FSharpLint.Core/FSharpLint.Core.fsproj(134,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
    /home/runner/work/FSharpLint/FSharpLint/src/FSharpLint.Console/FSharpLint.Console.fsproj(27,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
    /home/runner/work/FSharpLint/FSharpLint/tests/FSharpLint.Console.Tests/FSharpLint.Console.Tests.fsproj(17,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
    /home/runner/work/FSharpLint/FSharpLint/tests/FSharpLint.Benchmarks/FSharpLint.Benchmarks.fsproj(14,3): error MSB4019: The imported project "/home/runner/work/FSharpLint/FSharpLint/.paket/Paket.Restore.targets" was not found. Confirm that the expression in the Import declaration "../../.paket/Paket.Restore.targets" is correct, and that the file exists on disk.
        0 Warning(s)
        6 Error(s)
    ```

--- scripts/buildAndRelease.fsx
+++ scripts/buildAndRelease.fsx
@@ -1,3 +1,4 @@
+#r "nuget: Fake.DotNet.Paket"
 #r "nuget: Fake.DotNet.Cli"
 #load "common.fsx"
 #load "clean.fsx"
@@ -7,6 +8,12 @@ open Fake.DotNet
 open Common
 
 let properties = ("Version", nugetVersion) |> List.singleton |> PackageReleaseNotes
+
+Paket.restore (fun paketRestoreParam ->
+    { paketRestoreParam with
+        WorkingDir = Common.rootDir.FullName
+        ToolType = ToolType.CreateLocalTool()
+    })
 
 DotNet.build (fun p ->
     { p with
