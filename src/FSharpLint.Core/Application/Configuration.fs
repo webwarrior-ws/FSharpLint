ee08bdd12d0ea6e897cc0a8603ced7af55aaf7ab
webwarrior-ws <reg@webwarrior.ws>
2025-11-05 13:21:08 +0100

    NoAsyncRunSynchronouslyInLibrary: add config
    Add configuration for the rule and enable it by default.

--- src/FSharpLint.Core/Application/Configuration.fs
+++ src/FSharpLint.Core/Application/Configuration.fs
@@ -473,7 +473,8 @@ type Configuration =
       NoPartialFunctions:RuleConfig<NoPartialFunctions.Config> option
       SuggestUseAutoProperty:EnabledConfig option
       EnsureTailCallDiagnosticsInRecursiveFunctions:EnabledConfig option
-      FavourAsKeyword:EnabledConfig option }
+      FavourAsKeyword:EnabledConfig option
+      NoAsyncRunSynchronouslyInLibrary:EnabledConfig option}
 with
     static member Zero = {
         Global = None
@@ -568,6 +569,7 @@ with
         SuggestUseAutoProperty = None
         EnsureTailCallDiagnosticsInRecursiveFunctions = None
         FavourAsKeyword = None
+        NoAsyncRunSynchronouslyInLibrary = None
     }
 
 // fsharplint:enable RecordFieldNames
@@ -763,6 +765,7 @@ let flattenConfig (config:Configuration) =
                 config.SuggestUseAutoProperty |> Option.bind (constructRuleIfEnabled SuggestUseAutoProperty.rule)
                 config.EnsureTailCallDiagnosticsInRecursiveFunctions |> Option.bind (constructRuleIfEnabled EnsureTailCallDiagnosticsInRecursiveFunctions.rule)
                 config.FavourAsKeyword |> Option.bind (constructRuleIfEnabled FavourAsKeyword.rule)
+                config.NoAsyncRunSynchronouslyInLibrary |> Option.bind (constructRuleIfEnabled NoAsyncRunSynchronouslyInLibrary.rule)
             |]
 
     findDeprecation config deprecatedAllRules allRules
