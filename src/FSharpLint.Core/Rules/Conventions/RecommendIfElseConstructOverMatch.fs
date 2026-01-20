ed84208483e99672e9aff5e519d20068f3afa648
webwarrior-ws <reg@webwarrior.ws>
2026-01-20 12:11:31 +0100

    RecommendIfElseConstructOverMatch: fix rule

--- src/FSharpLint.Core/Rules/Conventions/RecommendIfElseConstructOverMatch.fs
+++ src/FSharpLint.Core/Rules/Conventions/RecommendIfElseConstructOverMatch.fs
@@ -4,27 +4,50 @@ open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 open FSharp.Compiler.Syntax
 open FSharp.Compiler.Text
+open FSharp.Compiler.CodeAnalysis
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
 
-let private isBasicControlFlow (synMatchClauses: List<SynMatchClause>) =
-    let isFirstClauseTarget firstClause =
-        match firstClause with
-        | SynMatchClause(SynPat.Named(_), _, _, _, _, _)
-        | SynMatchClause(SynPat.Const(_), _, _, _, _, _)
-        | SynMatchClause(SynPat.LongIdent(SynLongIdent _, _, _, SynArgPats.Pats [], _, _), _, _, _, _, _) -> true
-        | _ -> false
-
-    let isLastClauseTarget lastClause =
-        match lastClause with
-        | SynMatchClause(SynPat.Wild _, None, _, _, _, _) -> true
-        | _ -> false
-
-    synMatchClauses.Length = 2 && isFirstClauseTarget synMatchClauses.[0] && isLastClauseTarget synMatchClauses.[1]
-
 let runner args =
+    let isBasicControlFlow (synMatchClauses: List<SynMatchClause>) =
+        let isDiscriminatedUnionCase (names: list<string>) (symbolUseRange: range) =
+            match args.CheckInfo with
+            | Some fileCheckInfo -> 
+                let symbolUse =
+                    fileCheckInfo.GetSymbolUseAtLocation(
+                        symbolUseRange.StartLine,
+                        symbolUseRange.EndColumn,
+                        args.Lines.[symbolUseRange.StartLine],
+                        names)
+                match symbolUse with
+                | Some(usage) ->
+                    match usage.Symbol with
+                    | :? FSharp.Compiler.Symbols.FSharpUnionCase -> true
+                    | _ -> false
+                | _ -> false
+            | None -> false
+    
+        let isFirstClauseTarget firstClause =
+            match firstClause with
+            | SynMatchClause(SynPat.Const(_), _, _, _, _, _) -> true
+            | SynMatchClause(SynPat.Named(SynIdent.SynIdent(ident, _), _, _, range), _, _, _, _, _) -> 
+                not <| isDiscriminatedUnionCase (List.singleton ident.idText) range
+            | SynMatchClause(SynPat.LongIdent(SynLongIdent(ident, _, _), _, _, SynArgPats.Pats [], _, range), _, _, _, _, _) -> 
+                not <| isDiscriminatedUnionCase (ident |> List.map (fun id -> id.idText)) range
+            | _ -> false
+
+        let isLastClauseTarget lastClause =
+            match lastClause with
+            | SynMatchClause(SynPat.Wild _, None, _, _, _, _) -> true
+            | _ -> false
+
+        match synMatchClauses with
+        | [ firstClause; lastClause ] -> isFirstClauseTarget firstClause && isLastClauseTarget lastClause
+        | _ -> false
+
     match args.AstNode with
-    | AstNode.Expression(SynExpr.Match (_, _, synMatchClauses, range, _)) when isBasicControlFlow synMatchClauses ->
+    | AstNode.Expression(SynExpr.Match (_, _, synMatchClauses, range, _))
+        when isBasicControlFlow synMatchClauses ->
         { Range = range
           Message = Resources.GetString "RecommendIfElseConstructOverMatch"
           SuggestedFix = None
