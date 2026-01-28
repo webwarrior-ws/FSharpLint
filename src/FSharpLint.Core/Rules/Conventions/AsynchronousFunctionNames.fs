2131f68ead8379e84aa4ae63da92606be1f510fb
webwarrior-ws <reg@webwarrior.ws>
2026-01-28 14:11:19 +0100

    WIP: start implementing rules

--- src/FSharpLint.Core/Rules/Conventions/AsynchronousFunctionNames.fs
+++ src/FSharpLint.Core/Rules/Conventions/AsynchronousFunctionNames.fs
@@ -5,9 +5,52 @@ open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 open FSharpLint.Framework.Ast
 open FSharpLint.Framework.Rules
+open FSharp.Compiler.Syntax
+
+let (|HasAsyncPrefix|HasAsyncSuffix|HasNoAsyncPrefixOrSuffix|) (pattern: SynLongIdent) =
+    match List.tryLast pattern.LongIdent with
+    | Some name ->
+        if name.idText.StartsWith "Async" then
+            HasAsyncPrefix name.idText
+        elif name.idText.EndsWith "Async" then
+            HasAsyncSuffix name.idText
+        else
+            HasNoAsyncPrefixOrSuffix name.idText
+    | _ -> HasNoAsyncPrefixOrSuffix String.Empty
+
+let (|ReturnsTask|ReturnsAsync|ReturnsNonAsync|) (returnInfo: SynBindingReturnInfo) =
+    match returnInfo with
+    | SynBindingReturnInfo(SynType.LongIdent(SynLongIdent(typeIdent, _, _)), _, _, _) ->
+        match List.tryLast typeIdent with
+        | Some ident when ident.idText = "Async" -> ReturnsAsync
+        | Some ident when ident.idText = "Task" -> ReturnsTask
+        | _ -> ReturnsNonAsync
+    | _ -> ReturnsNonAsync
 
 let runner (args: AstNodeRuleParams) =
-    failwith "Not yet implemented"
+    match args.AstNode with
+    | AstNode.Binding (SynBinding (_, _, _, _, _, _, _, SynPat.LongIdent(HasNoAsyncPrefixOrSuffix name, _, _, _, _, identRange), returnInfo, _, _, _, _)) ->
+        match returnInfo with
+        | Some ReturnsTask ->
+            Array.singleton
+                {
+                    Range = identRange
+                    Message = $"This function returns Task. Consider naming it {name}Async"
+                    SuggestedFix = None
+                    TypeChecks = List.empty
+                }
+        | Some ReturnsAsync ->
+            Array.singleton
+                {
+                    Range = identRange
+                    Message = $"This function returns Async. Consider naming it Async{name}"
+                    SuggestedFix = None
+                    TypeChecks = List.empty
+                }
+        | _ -> 
+            // TODO: get type from args.CheckInfo
+            Array.empty
+    | _ -> Array.empty
 
 let rule =
     AstNodeRule
